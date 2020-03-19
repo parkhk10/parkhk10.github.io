ff14f9690dc3a54372d91fe2f70418f7da118581
Keren Park <parkhk10@gmail.com>
2020-03-18 23:23:44 -0400

    Updates

---
+++ precache-manifest.b40a35a9aa9e743aeb36864a2523696a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205378ab70b8446a717d7bcfa179dc57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4f4e839f9a2aa2b7641",
+    "url": "/static/css/main.9b346462.chunk.css"
+  },
+  {
+    "revision": "aca69a734360ced1e5ed",
+    "url": "/static/js/2.f27eb4b5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f27eb4b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f4e839f9a2aa2b7641",
+    "url": "/static/js/main.0f118314.chunk.js"
+  },
+  {
+    "revision": "fc89960afa630bfea928",
+    "url": "/static/js/runtime-main.57fe856e.js"
+  }
+]);
